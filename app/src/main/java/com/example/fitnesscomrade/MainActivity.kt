6b077d8f13dd0fd2f3ff320ab2743e52031b1073
Erick Resurreccion <72366477+ResurreccionErick@users.noreply.github.com>
2024-08-08 03:39:30 +0800

    Add Top bar

--- app/src/main/java/com/example/fitnesscomrade/MainActivity.kt
+++ app/src/main/java/com/example/fitnesscomrade/MainActivity.kt
@@ -1,24 +1,17 @@
 package com.example.fitnesscomrade
 
-import HomeScreen
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.fitnesscomrade.ui.screens.HomeScreen
 import com.example.fitnesscomrade.ui.screens.SplashScreen
 import com.example.fitnesscomrade.ui.theme.FitnessComradeTheme
 import kotlinx.coroutines.delay
--- app/src/main/java/com/example/fitnesscomrade/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/fitnesscomrade/ui/screens/HomeScreen.kt
@@ -1,6 +1,98 @@
-import androidx.compose.runtime.Composable
+package com.example.fitnesscomrade.ui.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fitnesscomrade.R
+
+@Preview
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
+    Scaffold(
+        topBar = {
+//            TopAppBar(
+//                title = { },
+//                modifier = Modifier.padding(1.dp)
+//            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 25.dp)
+                .background(Color.White), // Background color to make sure it matches the app theme
+            verticalArrangement = Arrangement.Top, // Align items to the top
+            horizontalAlignment = Alignment.CenterHorizontally // Center items horizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(65.dp)
+                        .clip(CircleShape),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.sample_pic),
+                        contentDescription = "profile pic"
+                    )
+                }
 
+                Text(buildAnnotatedString {
+                    append("Hello, ")
+                    withStyle(
+                        SpanStyle(
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                    ) {
+                        append("User")
+                    }
+                }, modifier = Modifier.padding(start = 10.dp))
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                BadgedBox(badge = {
+                    Badge(
+                        Modifier
+                            .clip(CircleShape)
+                            .background(Color.Red)
+                            .align(Alignment.BottomEnd)
+                    )
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Notifications,
+                        contentDescription = "notification icon",
+                        tint = Color.Black
+                    )
+                }
+            }
+        }
+    }
 }
