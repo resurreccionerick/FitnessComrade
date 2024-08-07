db2131493f9bdc77f0fd5a909d89139286faecb5
Erick Resurreccion <72366477+ResurreccionErick@users.noreply.github.com>
2024-08-08 02:58:40 +0800

    Add Splash Screen

--- app/src/main/java/com/example/fitnesscomrade/MainActivity.kt
+++ app/src/main/java/com/example/fitnesscomrade/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fitnesscomrade
 
+import HomeScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,9 +10,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.fitnesscomrade.ui.screens.SplashScreen
 import com.example.fitnesscomrade.ui.theme.FitnessComradeTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +29,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FitnessComradeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                InitializeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun InitializeApp() {
+    var showSplash by rememberSaveable { mutableStateOf(true) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitnessComradeTheme {
-        Greeting("Android")
+    LaunchedEffect(Unit) {
+        delay(2000) //seconds delay
+        showSplash = false
+    }
+
+    if (showSplash) {
+        SplashScreen()
+    } else {
+        HomeScreen()
     }
 }
---
+++ app/src/main/java/com/example/fitnesscomrade/ui/screens/SplashScreen.kt
@@ -0,0 +1,55 @@
+package com.example.fitnesscomrade.ui.screens
+
+import android.graphics.Paint.Align
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.fitnesscomrade.R
+import kotlinx.coroutines.delay
+
+@Preview
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.main_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(500.dp)
+            )
+
+            //progress bar
+            CircularProgressIndicator()
+
+
+            // Timer to transition from the splash screen
+            LaunchedEffect(Unit) {
+                delay(2000) // Delay for 2 seconds
+            }
+        }
+
+    }
+}
